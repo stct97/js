92dea7243f0e4de6d1bb2fbf526a60c6fe008878
stct97 <salvadorcruztabares@gmail.com>
2021-09-13 15:01:33 +0200

    add bs

--- oddPair/oddPair.js
+++ oddPair/oddPair.js
@@ -1,13 +1,13 @@
 function check(num) {
     if (num === '') {
-        return document.body.innerHTML += "<li class='list-group-item list-group-item-danger'>Put a number!</li>";
+        return document.body.innerHTML += "<li class='list-group-item list-group-item-danger text-center'>Put a number!</li>";
     }
     if (num == 0) {
-        return document.body.innerHTML += "<li class='list-group-item list-group-item-warning'>Zero is pair mathematically!</li>";
+        return document.body.innerHTML += "<li class='list-group-item list-group-item-warning text-center'>Zero is pair mathematically!</li>";
     }
     if (num % 2 == 0) {
-        return document.body.innerHTML += "<li class='list-group-item list-group-item-primary'>Pair</li>";
+        return document.body.innerHTML += "<li class='list-group-item list-group-item-primary text-center'>Pair</li>";
     } else {
-        return document.body.innerHTML += "<li class='list-group-item list-group-item-info'>Odd</li>";
+        return document.body.innerHTML += "<li class='list-group-item list-group-item-info text-center'>Odd</li>";
     }
 }
