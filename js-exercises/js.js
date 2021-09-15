2df1ab9168cf128302eca374a4520cda8890424a
stct97 <salvadorcruztabares@gmail.com>
2021-09-15 15:43:27 +0200

    add new codes

--- js-exercises/js.js
+++ js-exercises/js.js
@@ -4,8 +4,8 @@
  * @returns sum of 2 numbers
  */
 function sum() {
-    x = window.prompt("Put first number");
-    y = window.prompt("Put second number");
+    var x = window.prompt("Put first number");
+    var y = window.prompt("Put second number");
     return alert(parseFloat(x) + parseFloat(y))
 }
 /**
@@ -13,8 +13,8 @@ function sum() {
  * @returns ºFarenheit
  */
 function convarGrades() {
-    celsius = window.prompt("Put grades celsius");
-    farenheit = parseInt(celsius) * parseInt(9) / parseInt(5) + parseFloat(32);
+    var celsius = window.prompt("Put grades celsius");
+    var farenheit = parseInt(celsius) * parseInt(9) / parseInt(5) + parseFloat(32);
     return alert(celsius + "º grados celsius" + " is " + farenheit + "º Farenheit")
 }
 /**
@@ -22,7 +22,7 @@ function convarGrades() {
  * @returns result of division
  */
 function div() {
-    x = window.prompt("Put a num to div");
+    var x = window.prompt("Put a num to div");
     return alert(parseFloat(x) / parseFloat(10))
 }
 /**
@@ -30,7 +30,7 @@ function div() {
  * @returns alert with type the var
  */
 function identify() {
-    x = [1];
+    var x = [1];
     return alert(typeof x);
 }
 
@@ -39,18 +39,18 @@ function identify() {
  * This function show if a number is odd or pair. Doesn´t receive parametres
  * @returns an alert with string result
  */
-function oddPair(){
-    x = window.prompt("Input a number");
-    if (parseInt(x) % 2 === 0){
+function oddPair() {
+    var x = window.prompt("Input a number");
+    if (parseInt(x) % 2 === 0) {
         return alert("pair")
     } else return alert("odd")
 }
 /**
  * This function check if input number by user is div between 5 and 11. Doesn´t receive parametres
  */
-function checkDiv(){
-    x = window.prompt("Input a number");
-    if (parseInt(x)%parseInt(11) === 0  && parseInt(x)%parseInt(5) === 0){
+function checkDiv() {
+    var x = window.prompt("Input a number");
+    if (parseInt(x) % parseInt(11) === 0 && parseInt(x) % parseInt(5) === 0) {
         console.log("true")
     } else console.log("false")
 }
@@ -58,10 +58,31 @@ function checkDiv(){
  * This function dosen´t receive parametres and show 2 prompt
  * @returns result of which number is the highest
  */
-function checkWhichNumberIsHighest(){
-    x = window.prompt("Input a number");
-    y = window.prompt("Input a number");
-    if(x>y){
+function checkWhichNumberIsHighest() {
+    var x = window.prompt("Input a number");
+    var y = window.prompt("Input a number");
+    if (x > y) {
         return alert("First input is the highest")
     } else alert("Second input is the highest")
 }
+/**
+ * Same that last function but using a ternary operator
+ * @returns same result
+ */
+function checkWhichNumberIsHighestTernaryOperator() {
+    return (x = window.prompt("Input a number")) > (y = window.prompt("Input a number")) ? alert("First input is the highest") : alert("Second input is the highest");
+}
+/**
+function checkLetterOrNumber() {
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var x = window.prompt("Input a whatever");
+    var r;
+    for (var i = 0; i < 9; i++){
+        if(numbers === x){
+            r = "number";
+            break;
+        } else r = "leter"
+    }
+    return alert(r);
+}
+*/
