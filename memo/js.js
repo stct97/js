efa5cc3fe674db3d0299ce54d343411a5fa5da41
stct97 <salvadorcruztabares@gmail.com>
2021-10-06 13:28:15 +0200

    memo game img and html added

--- memo/js.js
+++ memo/js.js
@@ -5,14 +5,12 @@ var four = "four";
 var countX = 0;
 var countY = 0;
 var cont = 5;
+
+/**
+ * 
+ * @param {*} image 
+ */
 function change(image) {
-    console.log(1);
-    /*if (document.getElementById("1").value == "1") {
-        console.log(2);
-        document.getElementById("1").onclick = document.getElementById("1").src = "/img/linux.png";
-    } else if (document.getElementById("2") == "2") {
-        document.getElementById("2").onclick = document.getElementById("2").src = "/img/win.png"
-    }*/
     switch (image.id) {
         case "1":
             document.getElementById("1").onclick = document.getElementById("1").src = "/img/linux.png";
@@ -31,11 +29,11 @@ function change(image) {
             four = document.getElementById("4").src;
             break;
     }
-    console.log(one);
-    console.log(two);
-    console.log(three);
-    console.log(four);
 }
+
+/**
+ * 
+ */
 function checkWin() {
     console.log(3)
     if (one === four && countX === 0) {
@@ -48,6 +46,9 @@ function checkWin() {
     
 }
 
+/**
+ * 
+ */
 function contador(){
 	var contador = document.getElementById("contador");
 	contador.value = cont;
